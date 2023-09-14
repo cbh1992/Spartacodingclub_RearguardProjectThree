df035b33b6d5a4dde97f145d986f3831a024bbc2
cbh1992 <whgg9786@gmail.com>
2023-09-14 13:06:50 +0900

    Updated: 2023-09-14

--- app/src/main/java/com/android/rearguardprojectthree/BlankFragment.kt
+++ app/src/main/java/com/android/rearguardprojectthree/BlankFragment.kt
@@ -10,29 +10,80 @@ import android.view.ViewGroup
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class BlankFragment : Fragment() {
-    private var param1: String? = null
+class BlankFragment : Fragment() { private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("onCreate","Create Log Test Text(Fragment)")
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("onViewCreated","ViewCreated Log Test Text(Fragment)")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d("onStart","Start Log Test Text(Fragment)")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("onResume","Resume Log Test Text(Fragment)")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("onPause","Pause Log Test Text(Fragment)")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("onStop","Stop Log Test Text(Fragment)")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        Log.d("onSaveInstanceState","SaveInstanceState Log Test Text(Fragment)")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        Log.d("onDestroyView","DestroyView Log Test Text(Fragment)")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("onDestroy","Destroy Log Test Text(Fragment)")
+    }
+
+
+
+
     companion object {
-        @JvmStatic
+
         fun newInstance(param1: String, param2: String) =
-            BlankFragment2().apply {
+            BlankFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/android/rearguardprojectthree/MainActivity.kt
+++ app/src/main/java/com/android/rearguardprojectthree/MainActivity.kt
@@ -1,33 +1,45 @@
 package com.android.rearguardprojectthree
 
+import android.annotation.SuppressLint
 import android.app.appsearch.StorageInfo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //액티비티 전환
         val next = findViewById<Button>(R.id.button)
         next.setOnClickListener {
             val change = Intent(this,InnerActivity::class.java)
             startActivity(change)
         }
 
+        //프래그먼트 호출
         val frag = supportFragmentManager.beginTransaction()
-
         val fragclick = findViewById<Button>(R.id.button2)
         fragclick.setOnClickListener {
         setContentView(R.layout.fragment_blank2)
-                //frag.addToBackStack(null)
-                //frag.commit()
+                frag.addToBackStack(null)
+                frag.commit()
             }
 
+        //확장함수
+        fun showToast(text:String){
+            Toast.makeText(this,text,Toast.LENGTH_SHORT).show()
+        }
+        val  hello = findViewById<Button>(R.id.button3)
+        hello.setOnClickListener {
+            showToast("확장함수 테스트 메세지")
+        }
 
         Log.d("onCreate","Create Log Test Text(MainActivity)")
     }
